b8f5f5e66ad061057148e0dca7ae10b9f5af8098
Georgii <dzigarom@gmail.com>
2022-04-09 22:07:21 +0300

    Homework 2

--- Navigation/Navigation/ProfileViewController.swift
+++ Navigation/Navigation/ProfileViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  Profile.swift
 //  Navigation
 //
 //  Created by Георгий Маркарян on 09.04.2022.
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ProfileViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .lightGray
+        
     }
 
 
 }
-
